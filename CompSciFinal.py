3d5b3fa83db69cb381a170fe031033e85dd77ab5
Pi3th0n <Chasemansell@gmail.com>
2014-01-17 13:32:40 -0500

    Made the document!

---
+++ CompSciFinal.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+#   Authors: Chase Mansell & Stephen Simmons
+#   Created: Jan 17 2014
+#   Last Modified: Jan 17 2014
+#   About: This is our final project for CS402. We were inspired by the old game "Oregon Trail"
+#
+
+
