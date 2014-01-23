74f0d20ccce2ee88e015880e45a8c5f5ed48aafd
Pi3th0n <Chasemansell@gmail.com>
2014-01-23 15:24:14 -0500

    Playing with GUI and starting code for the game

--- CompSciFinal.py
+++ CompSciFinal.py
@@ -2,8 +2,19 @@
 #
 #   Authors: Chase Mansell & Stephen Simmons
 #   Created: Jan 17 2014
-#   Last Modified: Jan 17 2014
+#   Last Modified: Jan 23 2014
 #   About: This is our final project for CS402. We were inspired by the old game "Oregon Trail"
 #
 
 
+#  The space above this line should be where we define all of our functions
+#  This space below this line is where our game code will go
+
+location = 0
+# This variable will keep track of where in our program the player is, and should prevent the 
+# program from running code that we don't want to run.
+
+while location = 0
+    print("This is our introduction")
+    PlFName = str(input("What is your first name? "))
+    PlLName = str(input("What is your last name? "))
--- GUI.py
+++ GUI.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+# I'm pretty sure this is going to be way more trouble than it's worth
 
-print("\u250F" + 88*"\u2501" + "\u2513")
-print(14 * ("\u2503" + 88*" " + "\u2503"))
-print("\u2503" + 33*" " + "Holy shit I made a box!" + 32*" "+  "\u2503")
-print(13 * ("\u2503" + 88*" " + "\u2503"))
-print("\u2517" + 88*"\u2501" + "\u251B")
+print(
+("\u250F" + 88*"\u2501" + "\u2513\n") + 
+(14 * ("\u2503" + 88*" " + "\u2503\n")) + 
+("\u2503" + 33*" " + "Holy shit I made a box!" + 32*" "+  "\u2503\n") + 
+(13 * ("\u2503" + 88*" " + "\u2503\n")) + 
+("\u2517" + 88*"\u2501" + "\u251B")
+)
+
